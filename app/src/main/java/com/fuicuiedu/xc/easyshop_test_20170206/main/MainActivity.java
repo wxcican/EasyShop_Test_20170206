b9e232f94a9890aef7f35c3c2483d5efd11730a9
wxcican <wxcican@qq.com>
2017-02-07 11:30:03 +0800

    完成主页搭建

--- app/src/main/java/com/fuicuiedu/xc/easyshop_test_20170206/main/MainActivity.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_test_20170206/main/MainActivity.java
@@ -1,15 +1,132 @@
 package com.fuicuiedu.xc.easyshop_test_20170206.main;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
 
 import com.fuicuiedu.xc.easyshop_test_20170206.R;
+import com.fuicuiedu.xc.easyshop_test_20170206.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_test_20170206.main.me.MeFragment;
+import com.fuicuiedu.xc.easyshop_test_20170206.main.shop.ShopFragment;
+
+import butterknife.BindView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Unbinder unbinder;
+
+    @BindViews({R.id.tv_shop, R.id.tv_message, R.id.tv_mail_list, R.id.tv_me})
+    TextView[] textViews;
+
+    @BindView(R.id.main_toobar)
+    Toolbar toolbar;
+    @BindView(R.id.main_title)
+    TextView tv_title;
+    @BindView(R.id.viewpager)
+    ViewPager viewPager;
+
+    //点击2次返回，退出程序
+    private boolean isExit = false;
+    private ActivityUtils activityUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        unbinder = ButterKnife.bind(this);
+        activityUtils = new ActivityUtils(this);
+
+        //初始化视图
+        init();
+    }
+
+    //初始化视图
+    private void init() {
+        viewPager.setAdapter(unLoginAdapter);
+        // TODO: 2017/2/7 0007 判断用户是否登录，从而选择不同的适配器
+
+
+        //刚进来默认选择市场
+        textViews[0].setSelected(true);
+        //viewPager添加滑动监听，用于控制TextView的展示
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                //textView全部未选择
+                for (TextView textView : textViews){
+                    textView.setSelected(false);
+                }
+                //更改title，设置选择效果
+                tv_title.setText(textViews[position].getText());
+                textViews[position].setSelected(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    //未登录时的ViewPager适配器
+    private FragmentStatePagerAdapter unLoginAdapter = new FragmentStatePagerAdapter(getSupportFragmentManager()) {
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new ShopFragment();
+                case 1:
+                    return new UnLoginFragment();
+                case 2:
+                    return new UnLoginFragment();
+                case 3:
+                    return new MeFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return 4;
+        }
+    };
+
+    //点击2次返回，退出程序
+    @Override
+    public void onBackPressed() {
+        if (!isExit) {
+            isExit = true;
+            activityUtils.showToast("再摁一次退出程序");
+            //两秒内再次点击返回则退出
+            viewPager.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    isExit = false;
+                }
+            }, 2000);
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
     }
 }
---
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_test_20170206/main/me/MeFragment.java
@@ -0,0 +1,25 @@
+package com.fuicuiedu.xc.easyshop_test_20170206.main.me;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.fuicuiedu.xc.easyshop_test_20170206.R;
+
+/**
+ * 我的页面
+ */
+
+public class MeFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_me,container,false);
+        return view;
+    }
+
+}
