fe8b2278939eb086589be8a673b7e23f76ca3af5
wxcican <wxcican@qq.com>
2017-02-07 12:16:56 +0800

    加入登录和注册页面

--- app/src/main/java/com/fuicuiedu/xc/easyshop_test_20170206/main/me/MeFragment.java
+++ app/src/main/java/com/fuicuiedu/xc/easyshop_test_20170206/main/me/MeFragment.java
@@ -8,6 +8,11 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fuicuiedu.xc.easyshop_test_20170206.R;
+import com.fuicuiedu.xc.easyshop_test_20170206.commons.ActivityUtils;
+import com.fuicuiedu.xc.easyshop_test_20170206.user.LoginActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * 我的页面
@@ -15,11 +20,24 @@ import com.fuicuiedu.xc.easyshop_test_20170206.R;
 
 public class MeFragment extends Fragment {
 
+    private View view;
+    private ActivityUtils activityUtils;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_me,container,false);
+        if (view == null){
+            view = inflater.inflate(R.layout.fragment_me,container,false);
+            ButterKnife.bind(this,view);
+            activityUtils = new ActivityUtils(this);
+        }
         return view;
+    }
+
+    @OnClick({R.id.iv_user_head, R.id.tv_person_info, R.id.tv_login, R.id.tv_person_goods, R.id.tv_goods_upload})
+    public void onClick(View view) {
+        activityUtils.startActivity(LoginActivity.class);
+        // TODO: 2017/2/7 0007 需要判断是否登录，从而决定跳转位置 
     }
 
 }
